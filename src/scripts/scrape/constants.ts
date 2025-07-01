55474fc528854a4fe150ecea0b08bfe2fd03661f
kasahala <4k.kasahara@gmail.com>
2025-07-02 06:38:22 +0900

    取り込んだページの記録

--- src/scripts/scrape/constants.ts
+++ src/scripts/scrape/constants.ts
@@ -1,1 +1,9 @@
 export const BASE_URL = "https://idb.moepic.com/items/";
+
+// 次回はこのページも含めてそれ以降を取り込む
+const CURRENT_PAGE_NUMBER = {
+	lastUpdated: "2025-07-02",
+	defences: 1859,
+	shields: 65,
+	weapons: 365,
+};
