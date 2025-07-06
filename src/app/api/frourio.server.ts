d060076f70c8089d6cd850d2c55dcfc56ddc09c6
kasahala <4k.kasahara@gmail.com>
2025-07-07 04:07:48 +0900

    frourioをbiomeから除外して書き出し

--- src/app/api/frourio.server.ts
+++ src/app/api/frourio.server.ts
@@ -1,42 +1,39 @@
-import { type NextRequest, NextResponse } from "next/server";
-import type { z } from "zod";
-import type { frourioSpec } from "./frourio";
+import { NextRequest, NextResponse } from 'next/server';
+import type { z } from 'zod';
+import { frourioSpec } from './frourio';
 
 type SpecType = typeof frourioSpec;
 
-type Controller = {};
+type Controller = {
+};
 
 type MethodHandler = (req: NextRequest | Request) => Promise<NextResponse>;
 
-type ResHandler = {};
+type ResHandler = {
+};
 
 export const createRoute = (controller: Controller): ResHandler => {
-	return {};
+  return {
+
+  };
 };
 
 type FrourioError =
-	| {
-			status: 422;
-			error: string;
-			issues: { path: (string | number)[]; message: string }[];
-	  }
-	| { status: 500; error: string; issues?: undefined };
+  | { status: 422; error: string; issues: { path: (string | number)[]; message: string }[] }
+  | { status: 500; error: string; issues?: undefined };
 
 const createReqErr = (err: z.ZodError) =>
-	NextResponse.json<FrourioError>(
-		{
-			status: 422,
-			error: "Unprocessable Entity",
-			issues: err.issues.map((issue) => ({
-				path: issue.path,
-				message: issue.message,
-			})),
-		},
-		{ status: 422 },
-	);
+  NextResponse.json<FrourioError>(
+    {
+      status: 422,
+      error: 'Unprocessable Entity',
+      issues: err.issues.map((issue) => ({ path: issue.path, message: issue.message })),
+    },
+    { status: 422 },
+  );
 
 const createResErr = () =>
-	NextResponse.json<FrourioError>(
-		{ status: 500, error: "Internal Server Error" },
-		{ status: 500 },
-	);
+  NextResponse.json<FrourioError>(
+    { status: 500, error: 'Internal Server Error' },
+    { status: 500 },
+  );
--- src/app/api/item/frourio.client.ts
+++ src/app/api/item/frourio.client.ts
@@ -1,32 +1,29 @@
-import type { FrourioClientOption } from "@frourio/next";
-import type { z } from "zod";
-import { frourioSpec as frourioSpec_10pvn8k } from "./frourio";
+import type { FrourioClientOption } from '@frourio/next';
+import { z } from 'zod';
+import { frourioSpec as frourioSpec_10pvn8k } from './frourio'
 
 export const fc = (option?: FrourioClientOption) => ({
-	$url: $url_10pvn8k(option),
-	...methods_10pvn8k(option),
+  $url: $url_10pvn8k(option),
+  ...methods_10pvn8k(option),
 });
 
 export const $fc = (option?: FrourioClientOption) => ({
-	$url: {
-		post(): string {
-			const result = $url_10pvn8k(option).post();
+  $url: {
+    post(): string {
+      const result = $url_10pvn8k(option).post();
 
-			if (!result.isValid) throw result.reason;
+      if (!result.isValid) throw result.reason;
 
-			return result.data;
-		},
-	},
-	async $post(
-		req: Parameters<ReturnType<typeof methods_10pvn8k>["$post"]>[0],
-	): Promise<z.infer<(typeof frourioSpec_10pvn8k.post.res)[200]["body"]>> {
-		const result = await methods_10pvn8k(option).$post(req);
+      return result.data;
+    },
+  },
+  async $post(req: Parameters<ReturnType<typeof methods_10pvn8k>['$post']>[0]): Promise<z.infer<typeof frourioSpec_10pvn8k.post.res[200]['body']>> {
+    const result = await methods_10pvn8k(option).$post(req);
 
-		if (!result.isValid)
-			throw result.isValid === false ? result.reason : result.error;
+    if (!result.isValid) throw result.isValid === false ? result.reason : result.error;
 
-		return result.data.body;
-	},
+    return result.data.body;
+  },
 });
 
 export const fc_10pvn8k = fc;
@@ -34,136 +31,61 @@ export const fc_10pvn8k = fc;
 export const $fc_10pvn8k = $fc;
 
 const $url_10pvn8k = (option?: FrourioClientOption) => ({
-	post():
-		| { isValid: true; data: string; reason?: undefined }
-		| { isValid: false; data?: undefined; reason: z.ZodError } {
-		return {
-			isValid: true,
-			data: `${option?.baseURL?.replace(/\/$/, "") ?? ""}D:/Data/Develop/software/moe-equipment-simulator/src/app/api/item`,
-		};
-	},
+  post(): { isValid: true; data: string; reason?: undefined } | { isValid: false, data?: undefined; reason: z.ZodError } {
+    return { isValid: true, data: `${option?.baseURL?.replace(/\/$/, '') ?? ''}D:/Data/Develop/software/moe-equipment-simulator/src/app/api/item` };
+  },
 });
 
 const methods_10pvn8k = (option?: FrourioClientOption) => ({
-	async $post(req: {
-		body: z.infer<typeof frourioSpec_10pvn8k.post.body>;
-		init?: RequestInit;
-	}): Promise<
-		| {
-				ok: true;
-				isValid: true;
-				data: {
-					status: 200;
-					headers?: undefined;
-					body: z.infer<(typeof frourioSpec_10pvn8k.post.res)[200]["body"]>;
-				};
-				failure?: undefined;
-				raw: Response;
-				reason?: undefined;
-				error?: undefined;
-		  }
-		| {
-				ok: boolean;
-				isValid: false;
-				data?: undefined;
-				failure?: undefined;
-				raw: Response;
-				reason: z.ZodError;
-				error?: undefined;
-		  }
-		| {
-				ok: boolean;
-				isValid?: undefined;
-				data?: undefined;
-				failure?: undefined;
-				raw: Response;
-				reason?: undefined;
-				error: unknown;
-		  }
-		| {
-				ok?: undefined;
-				isValid: false;
-				data?: undefined;
-				failure?: undefined;
-				raw?: undefined;
-				reason: z.ZodError;
-				error?: undefined;
-		  }
-		| {
-				ok?: undefined;
-				isValid?: undefined;
-				data?: undefined;
-				failure?: undefined;
-				raw?: undefined;
-				reason?: undefined;
-				error: unknown;
-		  }
-	> {
-		const url = $url_10pvn8k(option).post();
+  async $post(req: { body: z.infer<typeof frourioSpec_10pvn8k.post.body>, init?: RequestInit }): Promise<
+    | { ok: true; isValid: true; data: { status: 200; headers?: undefined; body: z.infer<typeof frourioSpec_10pvn8k.post.res[200]['body']> }; failure?: undefined; raw: Response; reason?: undefined; error?: undefined }
+    | { ok: boolean; isValid: false; data?: undefined; failure?: undefined; raw: Response; reason: z.ZodError; error?: undefined }
+    | { ok: boolean; isValid?: undefined; data?: undefined; failure?: undefined; raw: Response; reason?: undefined; error: unknown }
+    | { ok?: undefined; isValid: false; data?: undefined; failure?: undefined; raw?: undefined; reason: z.ZodError; error?: undefined }
+    | { ok?: undefined; isValid?: undefined; data?: undefined; failure?: undefined; raw?: undefined; reason?: undefined; error: unknown }
+  > {
+    const url = $url_10pvn8k(option).post();
 
-		if (url.reason) return url;
+    if (url.reason) return url;
 
-		const parsedBody = frourioSpec_10pvn8k.post.body.safeParse(req.body);
+    const parsedBody = frourioSpec_10pvn8k.post.body.safeParse(req.body);
 
-		if (!parsedBody.success)
-			return { isValid: false, reason: parsedBody.error };
+    if (!parsedBody.success) return { isValid: false, reason: parsedBody.error };
 
-		const fetchFn = option?.fetch ?? fetch;
-		const result:
-			| { success: true; res: Response }
-			| { success: false; error: unknown } = await fetchFn(url.data, {
-			method: "POST",
-			...option?.init,
-			body: JSON.stringify(parsedBody.data),
-			...req.init,
-			headers: {
-				...option?.init?.headers,
-				"content-type": "application/json",
-				...req.init?.headers,
-			},
-		})
-			.then((res) => ({ success: true, res }) as const)
-			.catch((error) => ({ success: false, error }));
+    const fetchFn = option?.fetch ?? fetch;
+    const result: { success: true; res: Response } | { success: false; error: unknown } = await fetchFn(
+      url.data,
+      {
+        method: 'POST',
+        ...option?.init,
+        body: JSON.stringify(parsedBody.data),
+        ...req.init,
+        headers: { ...option?.init?.headers, 'content-type': 'application/json', ...req.init?.headers },
+      }
+    ).then(res => ({ success: true, res } as const)).catch(error => ({ success: false, error }));
 
-		if (!result.success) return { error: result.error };
+    if (!result.success) return { error: result.error };
 
-		switch (result.res.status) {
-			case 200: {
-				const resBody:
-					| { success: true; data: unknown }
-					| { success: false; error: unknown } = await result.res
-					.json()
-					.then((data) => ({ success: true, data }) as const)
-					.catch((error) => ({ success: false, error }));
+    switch (result.res.status) {
+      case 200: {
+        const resBody: { success: true; data: unknown } | { success: false; error: unknown } = await result.res.json().then(data => ({ success: true, data } as const)).catch(error => ({ success: false, error }));
 
-				if (!resBody.success)
-					return { ok: true, raw: result.res, error: resBody.error };
+        if (!resBody.success) return { ok: true, raw: result.res, error: resBody.error };
 
-				const body = frourioSpec_10pvn8k.post.res[200].body.safeParse(
-					resBody.data,
-				);
+        const body = frourioSpec_10pvn8k.post.res[200].body.safeParse(resBody.data);
 
-				if (!body.success)
-					return {
-						ok: true,
-						isValid: false,
-						raw: result.res,
-						reason: body.error,
-					};
+        if (!body.success) return { ok: true, isValid: false, raw: result.res, reason: body.error };
 
-				return {
-					ok: true,
-					isValid: true,
-					data: { status: 200, body: body.data },
-					raw: result.res,
-				};
-			}
-			default:
-				return {
-					ok: result.res.ok,
-					raw: result.res,
-					error: new Error(`Unknown status: ${result.res.status}`),
-				};
-		}
-	},
+        return {
+          ok: true,
+          isValid: true,
+          data: { status: 200, body: body.data },
+          raw: result.res,
+        };
+      }
+      default:
+        return { ok: result.res.ok, raw: result.res, error: new Error(`Unknown status: ${result.res.status}`) };
+    }
+  },
 });
+
